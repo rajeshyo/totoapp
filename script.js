802c61300693ae7b329dc34cce86763beda05fda
rajeshyo <rajesh95mailbox@gmail.com>
2026-05-29 19:24:09 +0530

    change env path

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ===== API Configuration =====
-// const API_BASE_URL = 'http://localhost:5000/api';
-const API_BASE_URL = 'https://totoapp.onrender.com';
+const API_BASE_URL = 'http://localhost:5000/api';
+// const API_BASE_URL = 'https://totoapp.onrender.com';
 
 
 // Helper function to make API calls with authorization
